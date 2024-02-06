1b21698ef596b817798967114fda8d3650c41bd3
guilhermeDOR <deorguilherme@gmail.com>
2024-02-05 21:54:08 -0300

    primeiro update. Reorganização do input de resultado para aparecimento após calcular. Ajudou a melhorar a responsividade por ocupar menos espaço.

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const button = document.getElementById('botao')
+const xButton = document.querySelector('.close-button')
+const mask = document.querySelector('.mask')
+const result = document.querySelector('.input-answer')
+
 
 
 function numberSorting() {
     const min = Math.ceil(document.getElementById('input-esquerdo').value)
     const max = Math.floor(document.getElementById('input-direito').value)
-    const result = document.querySelector('.input-answer')
+
 
     const finalValue = Math.floor(Math.random() * (max - min + 1)) + min;
-
+    
+    mask.style.visibility = 'visible';
     result.value = finalValue
+
 }
 
+function closeButton(){
+    mask.style.visibility = 'hidden';
+    button.style.zIndex = '2';
+}
 
 button.addEventListener('click', numberSorting);
+xButton.addEventListener('click', closeButton);
