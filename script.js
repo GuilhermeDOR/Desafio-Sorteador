cb1790d90c5842ebe02937252841884e92b4d616
guilhermeDOR <deorguilherme@gmail.com>
2024-02-06 20:29:22 -0300

    pequena alteração em relação ao evento de clickar e aparecer o resultado, sendo adicionado um requerimento de preenchimento correto dos campos para que aconteça o sorteio dos números

--- script.js
+++ script.js
@@ -9,17 +9,21 @@ function numberSorting() {
     const min = Math.ceil(document.getElementById('input-esquerdo').value)
     const max = Math.floor(document.getElementById('input-direito').value)
 
+    if (min <= max) {
 
-    const finalValue = Math.floor(Math.random() * (max - min + 1)) + min;
-    
-    mask.style.visibility = 'visible';
-    result.value = finalValue
+        const finalValue = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        mask.style.visibility = 'visible';
+        result.value = finalValue
+
+    } else {
+        alert('Preencha os campos de menor para menor!')
+    }
 
 }
 
-function closeButton(){
+function closeButton() {
     mask.style.visibility = 'hidden';
-    button.style.zIndex = '2';
 }
 
 button.addEventListener('click', numberSorting);
